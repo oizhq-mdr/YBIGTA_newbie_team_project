6542b2a4618c39c7351dff5e2f9192c57fdf065f
gamma4638 <stars3621@yonsei.ac.kr>
2025-01-16 14:57:05 +0900

    sync

---
+++ app/user/user_service.py
@@ -0,0 +1,34 @@
+from app.user.user_repository import UserRepository
+from app.user.user_schema import User, UserLogin, UserUpdate
+
+class UserService:
+    def __init__(self, userRepoitory: UserRepository) -> None:
+        self.repo = userRepoitory
+
+    def login(self, user_login: UserLogin) -> User:
+        user = self.repo.get_user_by_email(user_login.email)
+        if not user:
+            raise ValueError("User not Found.")
+        if user.password != user_login.password:
+            raise ValueError("Invalid ID/PW")
+        return user
+        
+    def register_user(self, new_user: User) -> User:
+        ## TODO
+        new_user = None
+        return new_user
+
+    def delete_user(self, email: str) -> User:
+        ## TODO        
+        deleted_user = None
+        return deleted_user
+
+
+
+    def update_user_pwd(self, user_update: UserUpdate) -> User:
+        user = self.repo.get_user_by_email(user_update.email)
+        if not user:
+            raise ValueError("User not Found.")
+        user.password = user_update.new_password
+        return self.repo.save_user(user)
+        
