16f81b3bb0b3f2bde5ee5c5377654cefb677399a
gamma4638 <stars3621@yonsei.ac.kr>
2025-01-16 15:53:28 +0900

    update router

--- app/user/user_router.py
+++ app/user/user_router.py
@@ -66,5 +66,26 @@ def delete_user(user_delete_request: UserDeleteRequest, service: UserService = D
 
 @user.put("/update-password", response_model=BaseResponse[User], status_code=status.HTTP_200_OK)
 def update_user_password(user_update: UserUpdate, service: UserService = Depends(get_user_service)) -> BaseResponse[User]:
-    ## TODO: 비밀번호 업데이트 기능 구현 필요
-    return None
+    """
+    사용자의 비밀번호를 업데이트합니다.
+
+    Args:
+        user_update (UserUpdate): 비밀번호 업데이트를 요청하는 사용자 정보와 새 비밀번호를 포함한 요청 객체
+        service (UserService, optional): UserService 인스턴스로, 사용자 관련 비즈니스 로직을 처리
+
+    Raises:
+        HTTPException: 
+            - 사용자를 찾을 수 없는 경우 404 에러 발생
+            - 그 외 다른 오류 발생 시 400 에러 발생
+
+    Returns:
+        BaseResponse[User]: 비밀번호가 업데이트된 사용자 정보를 포함한 응답 객체
+    """
+    try:
+        updated_user = service.update_user_pwd(user_update)
+        return BaseResponse(status="success", data=updated_user, message="Password updated successfully.")
+    except ValueError as e:
+
+        if "User not Found." in str(e):
+            raise HTTPException(status_code=404, detail=str(e))
+        raise HTTPException(status_code=400, detail=str(e))
