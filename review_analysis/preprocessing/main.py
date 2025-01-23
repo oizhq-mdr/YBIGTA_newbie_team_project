5356913ef13286154d8c5346a77a0679a9161e5e
oizhq-mdr <oilcanguitar@gmail.com>
2025-01-23 17:39:30 +0900

    preprocessing/main.py 수정

--- review_analysis/preprocessing/main.py
+++ review_analysis/preprocessing/main.py
@@ -3,21 +3,25 @@ import glob
 from argparse import ArgumentParser
 from typing import Dict, Type
 from review_analysis.preprocessing.base_processor import BaseDataProcessor
-from review_analysis.preprocessing.example_processor import ExampleProcessor
+from review_analysis.preprocessing.ssg_processor import SsgProcessor
 
 
 # 모든 preprocessing 클래스를 예시 형식으로 적어주세요. 
 # key는 "reviews_사이트이름"으로, value는 해당 처리를 위한 클래스
 PREPROCESS_CLASSES: Dict[str, Type[BaseDataProcessor]] = {
-    "reviews_example": ExampleProcessor,
+    "reviews_ssg": SsgProcessor
     # key는 크롤링한 csv파일 이름으로 적어주세요! ex. reviews_naver.csv -> reviews_naver
 }
 
-REVIEW_COLLECTIONS = glob.glob(os.path.join("..","..","database", "reviews_*.csv"))
+# REVIEW_COLLECTIONS = glob.glob(os.path.join("..","..","database", "reviews_*.csv"))
+REVIEW_COLLECTIONS = glob.glob(os.path.join("database", "reviews_*.csv"))
+print("REVIEW_COLLECTIONS", len(REVIEW_COLLECTIONS))
+
 
 def create_parser() -> ArgumentParser:
     parser = ArgumentParser()
-    parser.add_argument('-o', '--output_dir', type=str, required=False, default = "../../database", help="Output file dir. Example: ../../database")
+    # parser.add_argument('-o', '--output_dir', type=str, required=False, default = "../../database", help="Output file dir. Example: ../../database")
+    parser.add_argument('-o', '--output_dir', type=str, required=False, default = "database", help="Output file dir. Example: database")
     parser.add_argument('-c', '--preprocessor', type=str, required=False, choices=PREPROCESS_CLASSES.keys(),
                         help=f"Which processor to use. Choices: {', '.join(PREPROCESS_CLASSES.keys())}")
     parser.add_argument('-a', '--all', action='store_true',
