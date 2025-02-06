b1571d4e8aad4550d2246a883d4daa9017570fad
kulusevski <yosebjo53@gmail.com>
2025-02-06 15:00:16 +0900

    MongoDB에서 데이터 가져와 전처리하고, 다시 저장하는 API 생성

---
+++ app/review/review_router.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter
+from app.database.mongodb_connection import preprocess_reviews
+
+review = APIRouter()
+
+@review.post("/review/preprocess/{site_name}")
+async def preprocess_site_reviews(site_name: str):
+    """
+    특정 사이트(site_name)의 리뷰 데이터를 MongoDB에서 가져와 전처리하고, 다시 저장하는 API
+    """
+    result = preprocess_reviews(site_name)
+    return result
